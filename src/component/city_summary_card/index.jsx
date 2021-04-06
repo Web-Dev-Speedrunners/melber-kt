32ec7376e625036a630992ccd1b2680244249a85
Xuejin Gao <xuejingao@gmail.com>
2021-04-06 15:59:53 -0400

    Bulletpoint Layout setup

--- src/component/city_summary_card/index.jsx
+++ src/component/city_summary_card/index.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CitySummaryCard = ({ city }) => <h5>{city}</h5>;
+// const CitySummaryCard = ({ city }) => <h5>{city}</h5>;
+const CitySummaryCard = ({
+  city,
+  state,
+  latitude,
+  longitude,
+  population,
+  totalWages,
+}) => (
+  <ul>
+    <li>
+      {`City: ${city}`}
+    </li>
+    <li>
+      {`State: ${state}`}
+    </li>
+    <li>
+      {`Location: (${latitude}, ${longitude})`}
+    </li>
+    <li>
+      {`Population (Estimated): ${population}`}
+    </li>
+    <li>
+      {`Total Wages: ${totalWages}`}
+    </li>
+  </ul>
+);
 
 CitySummaryCard.propTypes = {
   city: PropTypes.string.isRequired,
-  // TODO: Add Adiditonal Props
+  state: PropTypes.string.isRequired,
+  latitude: PropTypes.number.isRequired,
+  longitude: PropTypes.number.isRequired,
+  population: PropTypes.number.isRequired,
+  totalWages: PropTypes.number.isRequired,
 };
 
 export default CitySummaryCard;
